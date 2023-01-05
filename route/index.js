434f707bb3cc508977313e24b2cf72b2c2c87b69
Nagesh Singh <nst321888@gmail.com>
2023-01-05 11:02:50 +0530

    hospital API completed  commit

--- route/index.js
+++ route/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
 const auth = require("../middleware/auth");
 
 const doctorRoute = require('./doctors');
 const reportRoute = require('./report');
 const patientRoute = require('./patients')
 
+router.get('/',(req,res)=>{ res.sendFile(path.join(__dirname,'../index.html')) })
 router.use("/doctors", doctorRoute)
 router.use("/patients", auth, patientRoute)
 router.use("/reports", auth, reportRoute)
